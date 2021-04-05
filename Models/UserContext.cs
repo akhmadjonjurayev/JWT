442bc194236d746e586cc4501f021a7b27313ee8
akhmadjonjurayev <formyproject1999@gmail.com>
2021-04-05 20:16:29 +0500

    JWT

---
+++ Models/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace WebApiRefReshToken.Models
+{
+    public class UserContext:DbContext
+    {
+        public UserContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<LoginModel> LoginModels { get; set; }
+    }
+}
